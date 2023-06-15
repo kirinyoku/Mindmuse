7067416e87a8abffca73d6a5661778d325dc6836
KIRIN <rudenokaleksandr21@gmail.com>
2023-06-15 11:12:54 +0300

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Feed from '@/components/feed';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello World</div>
-    </main>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Prompts is an open-source AI prompting tool for modern world to discover, create and share
+        creative prompts
+      </p>
+      <Feed />
+    </section>
   );
 }
